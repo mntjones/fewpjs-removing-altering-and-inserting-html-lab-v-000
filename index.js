698827acec2e1a3b21db55d11acc1fe773035944
Monica Tamaru Jones <mntamaru@gmail.com>
2019-11-12 21:57:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+main.remove();
+let newHeader = document.createElement("h1#victory");
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Monica is the champion";
